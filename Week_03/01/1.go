ac5630d202903f1ebdc160f0d5ad01a0a547d962
carlory <baofa.fan@daocloud.io>
2020-04-28 02:20:17 +0800

    01

---
+++ Week_03/01/1.go
@@ -0,0 +1,40 @@
+/*
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+package main
+
+// twoSum1 暴力破解
+func twoSum1(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+// twoSum2 哈希表，以空间换时间
+func twoSum2(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, v := range nums {
+		if value, ok := m[target-v]; ok {
+			return []int{value, i}
+		}
+		m[v] = i
+	}
+	return nil
+}
